35fb297cd6b3b9d8deec63054fc2ebaf8a761e38
03vmate <47510515+03vmate@users.noreply.github.com>
2021-02-24 10:32:29 +0100

    Finished UI and help page, implemented topright buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -277,5 +277,15 @@ namespace EasyMiner
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExitButton();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
